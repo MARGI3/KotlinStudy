31f7bf63032d4bbc1ba4e69fbf0e20a6a2d52b77
maji <maji@bilibili.com>
2018-12-10 17:59:30 +0800

    add sourceSets config & upgrade gradle version

--- app/src/main/java/com/margi/kotlinstudy/MainActivity.java
+++ app/src/main/java/com/margi/kotlinstudy/MainActivity.java
@@ -1,12 +1,18 @@
 package com.margi.kotlinstudy;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
+/**
+ * author : maji
+ * date   : 18/12/10
+ * mail   : maji@bilibili.com
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
