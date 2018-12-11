2f2a707545758b3c3968703faeb60bcf1bfb2e89
maji <maji@bilibili.com>
2018-12-11 15:35:14 +0800

    support AndroidStudio to run main() function

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # KotlinStudy
 Kotlin Study
+
+Run `main()` function in Android Studio reference [stackoverflow](https://stackoverflow.com/questions/16626810/can-android-studio-be-used-to-run-standard-java-projects/26196451#26196451)
+
---
+++ javamodule/src/main/java/com/margi/kotlinstudy/Main.kt
@@ -0,0 +1,20 @@
+package com.margi.kotlinstudy
+
+import com.margi.kotlinstudy.clazz.ConstructorT
+import com.margi.kotlinstudy.controlflow.ControlFlow
+
+/**
+ * author : maji
+ * date   : 18/12/11
+ * mail   : maji@bilibili.com
+ */
+object Main {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+//        val flow = ControlFlow()
+//        flow.fun4()
+
+        val constructorT = ConstructorT("def", 2)
+    }
+}
